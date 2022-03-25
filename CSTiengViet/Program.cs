562e7395dcc19df15a0d499accd8193bc16bb20d
Tien Phuc <leesinvn123@gmail.com>
2022-03-25 21:34:14 +0700

    Add project files.

---
+++ CSTiengViet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CSTiengViet
+{
+    internal class Program
+    {
+        public delegate void EventHandler(int a);
+        // Publisher
+        class UserInput
+        {
+            public EventHandler hello { get; set; }
+            public void Input()
+            {
+                do
+                {
+                    var input = Console.ReadLine();
+                    var result = Int32.Parse(input);
+
+                    hello?.Invoke(result);
+                } while (true);
+            }
+        }
+
+        class CalcSqrt
+        {
+            public void Subscriber(UserInput input)
+            {
+                input.hello = calcSqrt;
+            }
+            public void calcSqrt(int a)
+            {
+                Console.WriteLine($"The sqrt of {a} is {Math.Sqrt(a)}");
+            }
+        }
+
+        class Square
+        {
+            public void Subscriber(UserInput input)
+            {
+                input.hello = calcSquare;
+            }
+            public void calcSquare(int a)
+            {
+                Console.WriteLine($"The square of {a} is: {a * a}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var newHandler = new UserInput();
+            var newSqrt = new CalcSqrt();
+            var newSquare = new Square();
+            newSquare.Subscriber(newHandler);
+            newHandler.Input();
+        }
+
+    }
+}
+
