a643cd2d363b641fa1061d4de4818243be1714b8
Tien Phuc <leesinvn123@gmail.com>
2022-03-25 22:26:26 +0700

    Learning/[C#] Learning about event and EventHandler, Learning about Producer and Consumer method.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -4,56 +4,84 @@ namespace CSTiengViet
 {
     internal class Program
     {
-        public delegate void EventHandler(int a);
+        
+        class DataInput: EventArgs
+        {
+            public int number { get; set; }
+
+            public DataInput(int num) => number = num;
+        }
+
+        public event EventHandler hello;
         // Publisher
         class UserInput
         {
-            public EventHandler hello { get; set; }
+            public event EventHandler hello;
             public void Input()
             {
                 do
                 {
+                    Console.WriteLine("Please enter a number: ");
                     var input = Console.ReadLine();
                     var result = Int32.Parse(input);
 
-                    hello?.Invoke(result);
+                    hello?.Invoke(this, new DataInput(result));
                 } while (true);
             }
         }
 
+        // Subscriber
         class CalcSqrt
         {
             public void Subscriber(UserInput input)
             {
-                input.hello = calcSqrt;
+                input.hello += calcSqrt;
             }
-            public void calcSqrt(int a)
+            public void calcSqrt(object sender, EventArgs e)
             {
-                Console.WriteLine($"The sqrt of {a} is {Math.Sqrt(a)}");
+                var newArgument = (DataInput)e;
+                
+                Console.WriteLine($"The sqrt of {newArgument.number} is {Math.Sqrt(newArgument.number)}");
             }
         }
 
+        //Subscriber
         class Square
         {
             public void Subscriber(UserInput input)
             {
-                input.hello = calcSquare;
+                input.hello += calcSquare;
             }
-            public void calcSquare(int a)
+            public void calcSquare(object someObj, EventArgs square)
             {
-                Console.WriteLine($"The square of {a} is: {a * a}");
+                var newSquare = (DataInput)square;
+                Console.WriteLine($"The square of {newSquare.number} is: {newSquare.number * newSquare.number}");
             }
         }
 
         static void Main(string[] args)
         {
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine();
+                Console.WriteLine("Cancel application");
+            };
+
             var newHandler = new UserInput();
+
+            newHandler.hello += (object otherObj, EventArgs e) =>
+            {
+                var newArg = (DataInput)e;
+
+                Console.WriteLine($"Value of the lambda is called when subscribe data: {newArg.number}");
+            };
+
             var newSqrt = new CalcSqrt();
             var newSquare = new Square();
             newSquare.Subscriber(newHandler);
+            newSqrt.Subscriber(newHandler);
             newHandler.Input();
 
-            Console.WriteLine("Hello i'm learning C#");
         }
 
     }
