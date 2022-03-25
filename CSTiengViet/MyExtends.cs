6951757f4d678a45259537b5b8bf380dea74d0ae
Tien Phuc <leesinvn123@gmail.com>
2022-03-25 23:09:39 +0700

    Learning/[C#]: Learning about Extend method in C#

---
+++ CSTiengViet/MyExtends.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSTiengViet
+{
+    public static class MyExtends
+    {
+        public static double SquareDouble(this double number)
+        {
+            return number * number;
+        }
+    }
+}
--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -1,87 +1,20 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace CSTiengViet
 {
+    public delegate double squareDouble(double x);
     internal class Program
     {
-        
-        class DataInput: EventArgs
-        {
-            public int number { get; set; }
-
-            public DataInput(int num) => number = num;
-        }
-
-        public event EventHandler hello;
-        // Publisher
-        class UserInput
-        {
-            public event EventHandler hello;
-            public void Input()
-            {
-                do
-                {
-                    Console.WriteLine("Please enter a number: ");
-                    var input = Console.ReadLine();
-                    var result = Int32.Parse(input);
-
-                    hello?.Invoke(this, new DataInput(result));
-                } while (true);
-            }
-        }
-
-        // Subscriber
-        class CalcSqrt
-        {
-            public void Subscriber(UserInput input)
-            {
-                input.hello += calcSqrt;
-            }
-            public void calcSqrt(object sender, EventArgs e)
-            {
-                var newArgument = (DataInput)e;
-                
-                Console.WriteLine($"The sqrt of {newArgument.number} is {Math.Sqrt(newArgument.number)}");
-            }
-        }
-
-        //Subscriber
-        class Square
-        {
-            public void Subscriber(UserInput input)
-            {
-                input.hello += calcSquare;
-            }
-            public void calcSquare(object someObj, EventArgs square)
-            {
-                var newSquare = (DataInput)square;
-                Console.WriteLine($"The square of {newSquare.number} is: {newSquare.number * newSquare.number}");
-            }
-        }
-
         static void Main(string[] args)
         {
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                Console.WriteLine();
-                Console.WriteLine("Cancel application");
-            };
+            squareDouble test01;
+            test01 = MyExtends.SquareDouble;
 
-            var newHandler = new UserInput();
-
-            newHandler.hello += (object otherObj, EventArgs e) =>
-            {
-                var newArg = (DataInput)e;
-
-                Console.WriteLine($"Value of the lambda is called when subscribe data: {newArg.number}");
-            };
-
-            var newSqrt = new CalcSqrt();
-            var newSquare = new Square();
-            newSquare.Subscriber(newHandler);
-            newSqrt.Subscriber(newHandler);
-            newHandler.Input();
-
+            var newDouble = 12.5;
+            Console.WriteLine(test01(newDouble));
+            Console.WriteLine(newDouble.SquareDouble());
         }
 
     }
