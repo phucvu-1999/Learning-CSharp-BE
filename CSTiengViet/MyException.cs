5d88aced2ac9ddcf88b506271a6e4027220b3de1
Tien Phuc <leesinvn123@gmail.com>
2022-03-26 14:29:49 +0700

    Learning/[C#]: Learning Exception and custome exception in C# to catch the error and not stop the program.

---
+++ CSTiengViet/MyException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSTiengViet
+{
+    internal class MyException: Exception
+    {
+        public MyException(): base("Invalid Name !!!")
+        {
+
+        }
+
+       
+    }
+
+    public class MyException2 : Exception
+    {
+        public int age { get; set; }
+        public MyException2(int _age): base($"{_age} is not valid age !!!")
+        {
+             age = _age;
+        }
+    }
+}
--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -4,87 +4,40 @@ using System.Linq;
 
 namespace CSTiengViet
 {
-
-    class Student
+  internal class Program
+  {
+    static void Register(string name, int age)
     {
-        public readonly string studentId = "Hello 123";
+            if (string.IsNullOrEmpty(name))
+            {
+                Exception myException = new Exception("Name is not valid !!!");
+                throw new MyException();
+            }
 
-        public Student(string id)
-        {
-            studentId = id;
-        }
+            if(age < 18 || age > 100)
+            {
+                throw new MyException2(age);
+            }
 
+            Console.WriteLine($"Successfully register with name: {name} and age: {age}");
+    }
+    static void Main(string[] args)
+    {
+            try
+            {
+
+            Register("Tien Phuc", 10);
+            }catch (MyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }catch (MyException2 ex)
+            {
+                Console.WriteLine(ex.Message);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
     }
 
-    class Vector
-    {
-        public double X;
-        public double Y;
-
-        public Vector(double _x, double _y)
-        {
-            X = _x;
-            Y = _y;
-        }
-
-        public void Info()
-        {
-            Console.WriteLine($"The value of x: {X}, and y: {Y}");
-        }
-
-        public static Vector operator +(Vector v1, Vector v2)
-        {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-        }
-        public static Vector operator +(Vector v1, double v2)
-        {
-            return new Vector(v1.X + v2, v1.Y + v2);
-        }
-
-        public double this[int index]
-        {
-            get
-            {
-                if (index == 0)
-                {
-                    return X;
-                }
-                else if (index == 1)
-                {
-                    return Y;
-                }
-                else
-                {
-                    throw new Exception("Invalid index");
-                };
-
-            }
-
-            set
-            {
-                if (index == 0)
-                {
-                    X = value;
-                }
-                else if (index == 1)
-                {
-                    Y = value;
-                } else
-                {
-                    throw new Exception("Invalid index");
-                }
-            }
-        }
-        internal class Program
-        {
-            static void Main(string[] args)
-            {
-                var vector1 = new Vector(12, 13);
-                vector1[0] = 25;
-                vector1[1] = 30;
-                Console.WriteLine(vector1[3]);
-            }
-
-        }
-    }
+  }
 }
