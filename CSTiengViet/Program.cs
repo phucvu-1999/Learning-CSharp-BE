a2c76d65a39e95af9fcd3740f66bcdae00c78418
Tien Phuc <leesinvn123@gmail.com>
2022-03-27 23:33:45 +0700

    Learning/[C#]: learning about the Linq and query data in Linq.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -9,98 +9,67 @@ using System.Net.Http;
 
 namespace CSTiengViet
 {
-  internal class Program
-  {
-        static void DoSomething(int seconds, string message, ConsoleColor colors)
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+        public string[] Color { get; set; }
+        public int Brand { get; set; }
+        public Product(int _id, string _name, double _price, string[] _color, int _brand)
         {
-            lock(Console.Out)
+            Id = _id;
+            Name = _name;
+            Price = _price;
+            Color = _color;
+            Brand = _brand;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,3} {Name,12} {Price,5} {Brand,2} {string.Join(",", Color)}";
+        }
+    }
+
+    public class Brand
+    {
+        public string BrandName { get; set; }
+        public int Id { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newBrand = new List<Brand>()
             {
-                Console.ForegroundColor = colors;
-                Console.WriteLine("Start ... ");
-                Console.ResetColor();
-            }
+                new Brand{Id = 1, BrandName = "AAA"},
+                new Brand{Id = 2, BrandName = "BBB"},
+                new Brand{Id = 4, BrandName = "CCC"},
+            };
 
-            for(int i = 0; i < seconds; i++)
+            var products = new List<Product>()
             {
-                lock(Console.Out)
-                {
-                    Console.ForegroundColor = colors;
-                    Console.WriteLine($"{message}: {i}");
-                    System.Threading.Thread.Sleep(1000);
-                    Console.ResetColor();
-                }
-            }
+             new Product(1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
+             new Product(2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
+             new Product(3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
+             new Product(4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
+             new Product(5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
+             new Product(6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
+             new Product(7, "Tủ áo",      600, new string[] {"Trắng"},               3),
+            };
 
-            lock (Console.Out)
-            {
-                Console.ForegroundColor = colors;
-                Console.WriteLine("End ... ");
-                Console.ResetColor();
-            }
+            // Example about join in C#
+            var query = from product in products
+                        join brand in newBrand on product.Brand equals brand.Id
+                        select new
+                        {
+                            joinName = product.Name,
+                            joinPrice = product.Price,
+                            joinId = product.Id,
+                            joinBrand = brand.BrandName
+                        };
+            query.ToList().ForEach(x => Console.WriteLine(x));
         }
-        
-        static async Task Task2()
-        {
-            var task2 = new Task(() => DoSomething(3, "Task 2", ConsoleColor.Blue));
-            task2.Start();
-
-            await task2;
-
-            Console.WriteLine("Successfully imlement task 2");
-        }
-        static async Task Task3()
-        {
-            var task3 = new Task(() => DoSomething(3, "Task 3", ConsoleColor.White));
-            task3.Start();
-
-            await task3;
-            Console.WriteLine("Task 3 successfully implement");
-        }
-
-        static async Task<string> Task4()
-        {
-                       
-            var task4 = new Task<string>(() =>
-            {
-                DoSomething(5, "Task 4", ConsoleColor.White);
-                return "Return form task 4";
-            });
-
-            task4.Start();
-            var kq = await task4;
-            Console.WriteLine("Successfully implement task 4");
-            return kq;
-        }
-
-        static async Task<string> Task5()
-        {
-            var task5 = new Task<string>((object obj) =>
-            {
-                string t = (string)obj;
-                DoSomething(5, t, ConsoleColor.Yellow);
-
-                return "Result return from taks5";
-            }, "Task 5");
-
-            task5.Start();
-            var kq = await task5;
-            Console.WriteLine("Successfully implement the task 5");
-            return kq;
-        }
-
-        static async Task<string> GetWeb(string url)
-        {
-           var httpClient = new HttpClient();
-           HttpResponseMessage content = await httpClient.GetAsync(url);
-           string pageContent = await content.Content.ReadAsStringAsync();
-            
-            return pageContent;
-        }
-
-        static async Task Main(string[] args)
-        {
-            var pageContent = await GetWeb("https://google.com");
-            Console.WriteLine(pageContent);
-        }
-  }
+    }
 }
