b2c90d677928000d048720a1f2d17b7f106e128b
Tien Phuc <leesinvn123@gmail.com>
2022-03-28 00:00:52 +0700

    Learning/[C#]: Learning about the Nuget package management in C#.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -1,75 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Net.Http;
-
+using Newtonsoft.Json;
 
 namespace CSTiengViet
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
 
-        public double Price { get; set; }
-        public string[] Color { get; set; }
-        public int Brand { get; set; }
-        public Product(int _id, string _name, double _price, string[] _color, int _brand)
-        {
-            Id = _id;
-            Name = _name;
-            Price = _price;
-            Color = _color;
-            Brand = _brand;
-        }
-
-        public override string ToString()
-        {
-            return $"{Id,3} {Name,12} {Price,5} {Brand,2} {string.Join(",", Color)}";
-        }
-    }
-
-    public class Brand
-    {
-        public string BrandName { get; set; }
-        public int Id { get; set; }
-    }
     internal class Program
     {
+        class Product
+        {
+            public string Name { get; set; }
+            public DateTime Expiry { get; set; }
+            public string[] Sizes { get; set; }
+        }
         static void Main(string[] args)
         {
-            var newBrand = new List<Brand>()
-            {
-                new Brand{Id = 1, BrandName = "AAA"},
-                new Brand{Id = 2, BrandName = "BBB"},
-                new Brand{Id = 4, BrandName = "CCC"},
-            };
+            Console.WriteLine("Hello world");
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
 
-            var products = new List<Product>()
-            {
-             new Product(1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
-             new Product(2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
-             new Product(3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
-             new Product(4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
-             new Product(5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
-             new Product(6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
-             new Product(7, "Tủ áo",      600, new string[] {"Trắng"},               3),
-            };
+            string json = JsonConvert.SerializeObject(product);
+            // {
+            //   "Name": "Apple",
+            //   "Expiry": "2008-12-28T00:00:00",
+            //   "Sizes": [
+            //     "Small"
+            //   ]
+            // }
 
-            // Example about join in C#
-            var query = from product in products
-                        join brand in newBrand on product.Brand equals brand.Id
-                        select new
-                        {
-                            joinName = product.Name,
-                            joinPrice = product.Price,
-                            joinId = product.Id,
-                            joinBrand = brand.BrandName
-                        };
-            query.ToList().ForEach(x => Console.WriteLine(x));
+            string json1 = @"{
+                'Name': 'Iphone'
+                'Expiry': '2022-03-27T00:00:00',
+                'Sizes': ['small']
+            }";
+
+            Product convertPd = JsonConvert.DeserializeObject<Product>(json1);
+            Console.WriteLine(convertPd.Name);
         }
     }
 }
