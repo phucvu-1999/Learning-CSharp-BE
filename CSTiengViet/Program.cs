5e227ec4dacd09943ff33c5c002ca7e9ef815e22
Tien Phuc <leesinvn123@gmail.com>
2022-03-28 21:15:08 +0700

    Learning/[C#]: Learning how to reference a file with another Class exist outside.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -1,42 +1,16 @@
 ﻿using System;
 using Newtonsoft.Json;
+using MyLibrary;
 
 namespace CSTiengViet
 {
-
     internal class Program
     {
-        class Product
-        {
-            public string Name { get; set; }
-            public DateTime Expiry { get; set; }
-            public string[] Sizes { get; set; }
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Product product = new Product();
-            product.Name = "Apple";
-            product.Expiry = new DateTime(2008, 12, 28);
-            product.Sizes = new string[] { "Small" };
-
-            string json = JsonConvert.SerializeObject(product);
-            // {
-            //   "Name": "Apple",
-            //   "Expiry": "2008-12-28T00:00:00",
-            //   "Sizes": [
-            //     "Small"
-            //   ]
-            // }
-
-            string json1 = @"{
-                'Name': 'Iphone'
-                'Expiry': '2022-03-27T00:00:00',
-                'Sizes': ['small']
-            }";
-
-            Product convertPd = JsonConvert.DeserializeObject<Product>(json1);
-            Console.WriteLine(convertPd.Name);
+            double a = 4300000;
+            var result = ConvertMoneyToText.NumberToText(a);
+            Console.WriteLine(result);
         }
     }
 }
