712b82e7ac1be694c6e782a95cb0bec8f74d273c
Tien Phuc <leesinvn123@gmail.com>
2022-03-28 23:08:51 +0700

    Learning/[C#]: Learning Types and Attributes in C#.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using Newtonsoft.Json;
 using MyLibrary;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSTiengViet
 {
     internal class Program
     {
+
+        class User
+        {
+            [Required(ErrorMessage = "This field can not be null")]
+            [StringLength(20, ErrorMessage = "This field value has to be from 5 to 20 chars")]
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Email { get; set; }
+            public int PhoneNumber { get; set; }
+
+            public User(string _name, int _age, string _email, int _phoneNum)
+            {
+                Name = _name;
+                Age = _age;
+                Email = _email;
+                PhoneNumber = _phoneNum;
+            }
+
+            [Obsolete("This method no longer exist !!!")]
+            public void GetInfo()
+            {
+                Console.WriteLine(Name);
+            }
+        }
         static void Main(string[] args)
         {
-            double a = 4300000;
-            var result = ConvertMoneyToText.NumberToText(a);
-            Console.WriteLine(result);
+            var user1 = new User("asafjashfkjhask", 23, "asdasfas", 1241241);
+            Console.WriteLine(user1.Name);
         }
     }
 }
