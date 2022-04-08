ac5794f84bf6e0d7402708b4b1c011145ad39fc3
Tien Phuc <leesinvn123@gmail.com>
2022-04-08 21:15:47 +0700

    Learning/[C#]: Learning http server in C#.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -7,158 +7,23 @@ using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Configuration;
+using System.Net;
 
 namespace CSTiengViet
 {
     internal class Program
     {
-        interface IClassB
-        {
-            public void ActionB();
-        }
-
-        interface IClassC
-        {
-            public void ActionC();
-        }
-
-        class C : IClassC
-        {
-            public void ActionC() => Console.WriteLine("Action in class C");
-        }
-
-        class C1 : IClassC
-        {
-            public void ActionC() => Console.WriteLine("This is the implement of class C");
-        }
-
-        class B : IClassB
-        {
-            IClassC c_dependency;
-            public B(IClassC classC)
-            {
-                c_dependency = classC;
-            }
-            public void ActionB()
-            {
-                Console.WriteLine("Action in class B");
-                c_dependency.ActionC();
-            }
-        }
-
-        class B1 : IClassB
-        {
-            IClassC c_dependency;
-            string message;
-
-            public B1(IClassC _cDep, string _msg)
-            {
-                message = _msg;
-                c_dependency = _cDep;
-                Console.WriteLine("Action B1 is created!!!");
-            }
-
-            public void ActionB()
-            {
-                Console.WriteLine(message);
-                c_dependency.ActionC();
-            }
-
-        }
-        class A
-        {
-            IClassB b_dependency;
-
-            public A(IClassB classB)
-            {
-                b_dependency = classB;
-            }
-
-            public void ActionA()
-            {
-                Console.WriteLine("Action in class A");
-                b_dependency.ActionB();
-            }
-        }
-
-        class Horn
-        {
-            public void Beep() => Console.WriteLine("Beep Beep");
-        }
-
-        class Car
-        {
-            public Horn horn { get; set; }
-            public void Beep()
-            {
-                horn.Beep();
-            }
-
-        }
-
-        public class MyServiceOptions
-        {
-            public string data1 { get; set; }
-            public string data2 { get; set; }
-
-        }
-
-        public class MyService
-        {
-            public string data1 { get; set; }
-            public string data2 { get; set; }
-
-            public MyService(IOptions<MyServiceOptions> options)
-            {
-                var _options = options.Value;
-                data1 = _options.data1;
-                data2 = _options.data2;
-            }
-
-            public void PrintData() => Console.WriteLine($"{data1} /{data2}");
-
-        }
-
-        public class DogOptions
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-        }
-
-        public class Dogs
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-
-            public void PrintInfo() => Console.WriteLine($"{Name} are {Age}");
-
-            public Dogs(IOptions<DogOptions> options)
-            {
-                Name = options.Value.Name;
-                Age = options.Value.Age;
-            }
-        }
-
-
         static void Main(string[] args)
         {
-            try
-            {
+            var hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
 
-                IConfigurationRoot configurationRoot;
-                ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-                configBuilder.SetBasePath(System.IO.Directory.GetCurrentDirectory());
-                configBuilder.AddJsonFile("config.json");
+            string url = "https://www.youtube.com/watch?v=iJlAMzFy4yQ&list=PLwJr0JSP7i8BERdErX9Ird67xTflZkxb-&index=34";
+            var uri = new Uri(url);
 
-                configurationRoot = configBuilder.Build();
+            var ipHostEntry = Dns.GetHostEntry(uri.Host);
+            Console.WriteLine(ipHostEntry);
 
-                var readValue = configurationRoot.GetSection("section1").GetSection("key1").Value;
-                Console.WriteLine(readValue);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
         }
     }
 }
