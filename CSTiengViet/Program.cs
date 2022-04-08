4cf76e071e29facef8935c8be1b73113defe48c5
Tien Phuc <leesinvn123@gmail.com>
2022-04-08 22:08:00 +0700

    Learning/[C#]: Learning Dns, Ping, Uri in C#.

--- CSTiengViet/Program.cs
+++ CSTiengViet/Program.cs
@@ -8,22 +8,53 @@ using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Configuration;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CSTiengViet
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void ShowHeaders(HttpResponseHeaders headers)
         {
-            var hostName = Dns.GetHostName();
-            Console.WriteLine(hostName);
+            Console.WriteLine("Headers");
+            foreach (var header in headers)
+            {
+                Console.WriteLine($"Header key: {header.Key}, header value: {header.Value}");
+            }
+        }
+        public static async Task<string> GetPageContent(string url)
+        {
+            using var httpClient = new HttpClient();
 
-            string url = "https://www.youtube.com/watch?v=iJlAMzFy4yQ&list=PLwJr0JSP7i8BERdErX9Ird67xTflZkxb-&index=34";
-            var uri = new Uri(url);
+            //httpClient.DefaultRequestHeaders.Add("")
 
-            var ipHostEntry = Dns.GetHostEntry(uri.Host);
-            Console.WriteLine(ipHostEntry);
+            HttpResponseMessage message = await httpClient.GetAsync(url);
+            //message.Headers
+            ShowHeaders(message.Headers);
 
+            string html = await message.Content.ReadAsStringAsync();
+
+            return html;
+        }
+        static async Task Main(string[] args)
+        {
+            // Http request: http client 
+            try
+            {
+                string url = "https://www.facebook.com";
+                string task = await GetPageContent(url);
+
+
+                //Console.WriteLine(task);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
